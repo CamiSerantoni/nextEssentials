723feb71aebafe5eab0ee8510aa2d96eecdb0c51
Camila Serantoni <camila.serantoni@blue.cl>
2025-01-30 23:58:29 -0300

    ading more features

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,16 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import Link from "next/link";
 import classes from "./page.module.css";
-import { useEffect } from "react";
 
-export default function MealsPage() {
+import { getMeals } from "@/lib/meals";
 
-   
-    return <>
+export default async function MealsPage() {
+
+   const meals= await getMeals();
+
+    return(
+
+<>
      <header className={classes.header}>
         <h1>
             Delicious  meals, created <span className={classes.highlight}>by you</span>
@@ -17,8 +21,10 @@ export default function MealsPage() {
         </p>
      </header>
      <main className={classes.main}>
-    <MealsGrid meals={[]} />
+    <MealsGrid meals={meals} />
 
      </main>
 
-    </> }
+    </>
+
+    )  }
