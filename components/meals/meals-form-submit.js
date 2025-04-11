2724140a1178e3d88d9c8a2bf8d4b72be9ca1d26
CamiSerantoni <camila.serantonir@gmail.com>
2025-04-11 08:33:28 -0400

    triggering cache validations

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -3,7 +3,7 @@ import { useFormStatus } from "react-dom";
 
 export default function MealsFormSubmit({ isSubmitting }) {
     const { pending} =  useFormStatus()
-    status.pending
+    // status.pending
 
 return <button disabled={pending} >
     {pending ? "Submitting..." : "Share Meal"}
